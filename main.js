27c31b6536fb093bfc657964513f2a0118ac7349
kamil_nurkowski <kamill1983@gmail.com>
2020-10-04 18:16:39 +0200

    Adding all trening files

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstElement = document.getElementById("first_h1");
+console.log(firstElement);
+
+const secoundElement = document.querySelector("#first_h2");
+console.log(secoundElement);
+
+const thirdElement = document.querySelectorAll("* h3")
+console.log(thirdElement);
+
+// secoundElement.addEventListener();
